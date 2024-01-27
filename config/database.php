f86b8a93178ee8454f2db7126c4243f6a4f4e511
Asep Yaman Suryaman <119494596+Hathappend@users.noreply.github.com>
2024-01-27 16:17:21 +0700

    Update database.php
    change database config

--- config/database.php
+++ config/database.php
@@ -45,7 +45,6 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -59,11 +58,11 @@ return [
             'strict' => true,
             'engine' => null,
             'modes' => [
+                'ONLY_FULL_GROUP_BY',
                 'STRICT_TRANS_TABLES',
                 'NO_ZERO_IN_DATE',
                 'NO_ZERO_DATE',
                 'ERROR_FOR_DIVISION_BY_ZERO',
-                'NO_AUTO_CREATE_USER',
                 'NO_ENGINE_SUBSTITUTION'
             ],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
