a461993f27af816db90662c2bb18ecc11d27dad5
Hathappend <asepy183@gmail.com>
2024-01-28 12:33:59 +0700

    change session key user to name in resources/views/home/home.blade.php

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -3,7 +3,7 @@
 
 <section id="shop">
     <div class="container p-5">
-        <h3 class="title">WELCOME {{ session()->get('user') ?? '' }} TO U-CANTEEN</h3>
+        <h3 class="title">WELCOME {{ session()->get('name') ?? '' }} TO U-CANTEEN</h3>
         <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item active" aria-current="page">Beranda</li>
