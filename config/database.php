26e6ad646e13113c6b369a8a34c18cd11970a1a5
Hathappend <asepy183@gmail.com>
2024-02-06 13:10:16 +0700

    change config/database.php

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
                 'NO_ENGINE_SUBSTITUTION'
             ],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/ssl/ca.pem',
             ]) : [],
         ],
 
