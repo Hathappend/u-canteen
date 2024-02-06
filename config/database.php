c783789ca32ede0375c6f32cc6b387bdeb1cefa9
Hathappend <asepy183@gmail.com>
2024-02-06 13:21:47 +0700

    change config/database.php

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
+            'prefix' => '3dGdvSNYhRUgpJn',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
